c42bfe4f06c1748f00164b0641bb944fa9f59d9b
Axyz7 <nautiyalaryan9406@gmail.com>
2026-03-17 14:11:57 +0530

    made food data model with gson for converting name

---
+++ composeApp/src/androidMain/kotlin/org/troikas/main/network/foodmodel.kt
@@ -0,0 +1,19 @@
+package org.troikas.main.network
+
+import com.google.gson.annotation.SerializedName 
+
+data class FoodResponse(
+    val code: String,
+    val product: ProductDetails?
+)
+
+data class ProductDetails(
+    @SerializedName("product_name") val productName: String?,
+    @SerializedName("ingredients_text") val ingredientsText: String?,
+    val nutrients: Nutrients?
+)
+
+data class Nutrients(
+    @SerializedName("sugars_100g") val sugarGrams: Double?,
+    @SerializedName("proteins_100g") val proteinsGrams: Double?   
+)
