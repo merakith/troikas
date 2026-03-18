0f15fe033e1f79c2405157d5e331651f9b7567f2
Axyz7 <nautiyalaryan9406@gmail.com>
2026-03-18 22:24:00 +0530

    implemented FoodRepository with try catch and IO dispatcher

---
+++ composeApp/src/androidMain/kotlin/org/troikas/main/network/FoodRepository.kt
@@ -0,0 +1,20 @@
+package org.troikas.main.network
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import com.yourteam.smartgrahak.network.RetrofitClient
+
+class FoodRepository{
+    suspend fun getProduct(barcode:String): ProductDetails?{
+        //forces execution from UI thread to background thread pool
+        return withContext(Dispatchers.IO){
+            try{
+                val response = RetrofitClient.apiService.getProductByBarcode(barcode)
+                response.product
+            } catch(e:Exception{
+                e.printStackTrace()
+                null
+            }
+        }
+    }
+}
