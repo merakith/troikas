59704898ccf00dfeda14bd02cc0165a8ad7e35a4
Axyz7 <nautiyalaryan9406@gmail.com>
2026-03-18 14:20:05 +0530

    built retrofit client singleton with lazy initialization

---
+++ composeApp/src/androidMain/kotlin/org/troikas/main/network/RetrofitClient.kt
@@ -0,0 +1,19 @@
+package com.yourteam.smartgrahak.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient{
+    private const val BASE_URL="https://world.openfoodfacts.org/"
+
+    //this will only run when something asks for retrofit
+    private val retrofit: Retrofit by lazy{
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    val apiService: OpenFoodApi by lazy{
+        retrofit.create(OpenFoodApi::class.java)
+    }
+}
