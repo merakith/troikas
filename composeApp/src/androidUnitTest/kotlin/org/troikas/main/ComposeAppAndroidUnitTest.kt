380dc76e6f96bff3c81f3c412847b98b4424592f
Axyz7 <nautiyalaryan9406@gmail.com>
2026-03-21 12:04:08 +0530

    ci: implement android unit testing without build process

--- composeApp/src/androidUnitTest/kotlin/org/troikas/main/ComposeAppAndroidUnitTest.kt
+++ composeApp/src/androidUnitTest/kotlin/org/troikas/main/ComposeAppAndroidUnitTest.kt
@@ -1,12 +1,30 @@
 package org.troikas.main
 
 import kotlin.test.Test
-import kotlin.test.assertEquals
+import kotlinx.coroutines.runBlocking
 
 class ComposeAppAndroidUnitTest {
 
     @Test
-    fun example() {
-        assertEquals(3, 1 + 2)
+    fun testOpenFoodFactsEngine() {
+        runBlocking{
+            println("booting repository")
+            val repo=FoodRepository()
+
+            val testBarcode="8901058002346"
+            println("sending request")
+
+            val product=repo.getProduct(testBarcode)
+
+            println("server response:")
+            if(product!=null){
+                println("success")
+                println("Product Name: ${product.productName}")
+                println("Ingredients: ${product.ingredientsText}")
+            }else{
+                println("failed,product is null")
+            }
+            println("done")
+        }
     }
 }
