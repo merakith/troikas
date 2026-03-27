64e110cf51c0554abc29309f2ebe6f03b960a79f
Axyz7 <nautiyalaryan9406@gmail.com>
2026-03-27 09:04:33 +0530

    chore: typo

--- composeApp/src/androidMain/kotlin/org/troikas/main/repository/IngredientRepository.kt
+++ composeApp/src/androidMain/kotlin/org/troikas/main/repository/IngredientRepository.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.withContext
 import org.troikas.main.database.IngredientClassification
 import org.troikas.main.database.IngredientDao
 
-class IngredienntRepository(private val ingredientDao: IngredientDao){
+class IngredientRepository(private val ingredientDao: IngredientDao){
     //takes list of cleaned names and checks the local db
     suspend fun analyzeIngredients(cleanedNames: List<String>):List<IngredientClassification>{
         return withContext(Dispatchers.IO){
