5c87af8fc69496b6c0bf177adf952c82df4ebb7c
Axyz7 <nautiyalaryan9406@gmail.com>
2026-03-28 21:26:11 +0530

    feat: implement a function managing both repositories for local DB and OpenFoodFacts API

---
+++ composeApp/src/androidMain/kotlin/org/troikas/main/utils/AnalyzeProductUseCase.kt
@@ -0,0 +1,19 @@
+package org.troikas.main.utils
+
+import org.troikas.main.network.FoodRepository
+import org.troikas.main.database.IngredientRepository
+import org.troikas.main.database.IngredientClassification
+
+class AnalyzeProductUseCase(
+    private val foodrepo: FoodRepository
+    private val ingredientrepo: IngredientRepository
+    private val parser:IngredientParser
+){
+    suspend fun execute(barcode:String): List<IngredientClassification>{
+        val product=foodrepo.getProduct(barcode)?: return emptyList()
+        val rawIngredients= product.ingredientsText
+        if(rawIngredients.isNullOrEmpty()){ return emptyList()}
+        val cleanList= parser.parseIngredients(rawIngredients)
+        val localResults=ingredientrepo.analyzeIngredients(cleanList)
+        return localResults
+    }
