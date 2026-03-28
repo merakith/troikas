713f9e3796d00d16618a0b850c75d2b56b887575
Axyz7 <nautiyalaryan9406@gmail.com>
2026-03-28 18:26:44 +0530

    feat: add IngredientParser to clean raw strings

---
+++ composeApp/src/androidMain/kotlin/org/troikas/main/utils/stringCleaner.kt
@@ -0,0 +1,15 @@
+package org.troikas.main.utils
+
+class IngredientParser{
+    fun parseIngredients(rawString: String): List<String>{
+        return rawString
+        //1.removed bracketed content
+            .replace(Regex("\\(.*?\\)"), "")   
+        //2. chop list
+            .split(",")
+        //3.trim whitespaces
+            .map{it.trim()}
+        //4.remove blanks
+            .filter{it.isNotEmpty()}
+    }
+}
