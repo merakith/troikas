3879032837e2d4173aeff42fea4daa85337e5ad3
Axyz7 <nautiyalaryan9406@gmail.com>
2026-03-30 09:38:02 +0530

    feat: replace brackets with comma to account for ingredients like preservatives(911,420)

--- composeApp/src/androidMain/kotlin/org/troikas/main/utils/StringParser.kt
+++ composeApp/src/androidMain/kotlin/org/troikas/main/utils/StringParser.kt
@@ -3,8 +3,13 @@ package org.troikas.main.utils
 class IngredientParser{
     fun parseIngredients(rawString: String): List<String>{
         return rawString
-            .replace(Regex("\\(.*?\\)"), "")   
-            .split(",","-", " and ", " And ", " AND ")
+            .replace("(",",")
+            .replace("[",",")   
+            .replace("{",",")   
+            .replace(")",",")   
+            .replace("}",",")   
+            .replace("]",",")      
+            .split(",","-", " and ", " And ", " AND ", ":")
             .map{it.trim()}
             .filter{it.isNotEmpty()}
     }
