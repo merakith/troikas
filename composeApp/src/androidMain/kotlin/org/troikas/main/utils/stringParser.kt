7a4f8d7b0f06e66a6b886690a83e60baaa391bf7
Axyz7 <nautiyalaryan9406@gmail.com>
2026-04-09 22:25:02 +0530

    feat: implement ingredient selection by checking if there's a number inside an ingredient on an index

---
+++ composeApp/src/androidMain/kotlin/org/troikas/main/utils/stringParser.kt
@@ -0,0 +1,18 @@
+package org.troikas.main.utils
+
+class IngredientParser {
+    fun stringParser(rawString: String): List<String> {
+        return Regex("\\((.*?)\\)")
+                .replace(rawString) { matchResult ->
+                    val textInside =
+                            matchResult.groupValues[
+                                    1] // numbers(if there are any) or whatever is nside the
+                    // brackets
+                    if (textInside.any { it.isDigit() }) ",$textInside"
+                    else "" // if it's a number, add it, or discard it
+                }
+                .split(",", "-", ":", " and ", " And ", " AND ")
+                .map { it.trim().lowercase() }
+                .filter { it.isNotEmpty() }
+    }
+}
