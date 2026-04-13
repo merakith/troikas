64ccd79f3e50a82537e89cba8817a0dbc390b633
Axyz7 <nautiyalaryan9406@gmail.com>
2026-04-13 07:52:34 +0530

    refactor: rename files accordingly

--- composeApp/src/androidMain/kotlin/org/troikas/main/utils/AnalyzeProductUseCase.kt
+++ composeApp/src/androidMain/kotlin/org/troikas/main/utils/AnalyzeProductUseCase.kt
@@ -1,20 +1,20 @@
 package org.troikas.main.utils
 
-import org.troikas.main.network.FoodRepository
-import org.troikas.main.database.IngredientRepository
 import org.troikas.main.database.IngredientClassification
+import org.troikas.main.database.IngredientRepository
+import org.troikas.main.network.FoodRepository
 
 class AnalyzeProductUseCase(
-    private val foodrepo: FoodRepository,
-    private val ingredientrepo: IngredientRepository,
-    private val parser:IngredientParser
-){
-    suspend fun execute(barcode:String): List<IngredientClassification>{
-        val product=foodrepo.getProduct(barcode)?: return emptyList()
-        val rawIngredients= product.ingredientsText
-        if(rawIngredients.isNullOrEmpty())return emptyList()
-        val cleanList= parser.parseIngredients(rawIngredients)
-        val localResults=ingredientrepo.analyzeIngredients(cleanList)
+        private val foodrepo: FoodRepository,
+        private val ingredientrepo: IngredientRepository,
+        private val parser: stringParser
+) {
+    suspend fun execute(barcode: String): List<IngredientClassification> {
+        val product = foodrepo.getProduct(barcode) ?: return emptyList()
+        val rawIngredients = product.ingredientsText
+        if (rawIngredients.isNullOrEmpty()) return emptyList()
+        val cleanList = parser.stringParser(rawIngredients)
+        val localResults = ingredientrepo.analyzeIngredients(cleanList)
         return localResults
     }
 }
