c9992a9aba8a99a443e46217b3cc740e441cf609
Axyz7 <nautiyalaryan9406@gmail.com>
2026-04-18 12:22:36 +0530

    feat: add a bigger data class to send whole product information to viewmodel

--- composeApp/src/androidMain/kotlin/org/troikas/main/utils/AnalyzeProductUseCase.kt
+++ composeApp/src/androidMain/kotlin/org/troikas/main/utils/AnalyzeProductUseCase.kt
@@ -3,18 +3,25 @@ package org.troikas.main.utils
 import org.troikas.main.database.IngredientClassification
 import org.troikas.main.database.IngredientRepository
 import org.troikas.main.network.FoodRepository
+import org.troikas.main.network.Product
+
+
+data class ProductUseCase(
+    val product: Product,
+    val classification: List<IngredientClassification>
+)
 
 class AnalyzeProductUseCase(
         private val foodrepo: FoodRepository,
         private val ingredientrepo: IngredientRepository,
         private val parser: stringParser
 ) {
-    suspend fun execute(barcode: String): List<IngredientClassification> {
-        val product = foodrepo.getProduct(barcode) ?: return emptyList()
-        val rawIngredients = product.ingredientsText
-        if (rawIngredients.isNullOrEmpty()) return emptyList()
+    suspend fun execute(barcode: String): ProductUseCase? {
+        val product = foodrepo.getProduct(barcode) ?: return null
+        val rawIngredients = product.ingredientsText //convert
+        if (rawIngredients.isNullOrEmpty()) return null
         val cleanList = parser.stringParser(rawIngredients)
         val localResults = ingredientrepo.analyzeIngredients(cleanList)
-        return localResults
+        return ProductUseCase(product, localResults)
     }
 }
