dd86a1ccd301847d9c8769731471b5db3e500ff9
Axyz7 <nautiyalaryan9406@gmail.com>
2026-04-18 17:11:55 +0530

    feat: add way to pass database to syncworker

--- composeApp/src/androidMain/kotlin/org/troikas/main/database/AppDatabase.kt
+++ composeApp/src/androidMain/kotlin/org/troikas/main/database/AppDatabase.kt
@@ -1,16 +1,35 @@
 package org.troikas.main.database
 
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
-//config
+// config
 @Database(
-    entities=[IngredientClassification::class],  //register tables here
-    version=1,  // if we add another column, we change this
-    exportSchema=false  //shuts up the compiler about missing schema folders
+        entities = [IngredientClassification::class], // register tables here
+        version = 1, // if we add another column, we change this
+        exportSchema = false // shuts up the compiler about missing schema folders
 )
-abstract class AppDatabase: RoomDatabase(){
+abstract class AppDatabase : RoomDatabase() {
     // this abstract function tells the room to auto generate the code
     // to connect DAO queries to this specific database.
     abstract fun ingredientDao(): IngredientDao
+
+    companion object {
+        @Volatile private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE
+                    ?: synchronized(this) { // create db if instance is null
+                        val instance =
+                                Room.databaseBuilder(
+                                        context.applicationContext,
+                                        AppDatabase::class.java,
+                                        "troikas_database" // name of local sql file
+                                ).build()
+                                INSTANCE=instance
+                                instance
+                    }
+        }
+    }
 }
