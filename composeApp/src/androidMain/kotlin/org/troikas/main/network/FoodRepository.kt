f825554959696a875978811d6e6077d17e4e507f
Dhananjay <itsmrnatural@duck.com>
2026-04-21 15:50:02 +0530

    refactor: simplify getProduct method and enhance queryProduct error handling
    Signed-off-by: Dhananjay <itsmrnatural@duck.com>

--- composeApp/src/androidMain/kotlin/org/troikas/main/network/FoodRepository.kt
+++ composeApp/src/androidMain/kotlin/org/troikas/main/network/FoodRepository.kt
@@ -2,48 +2,36 @@ package org.troikas.main.network
 
 import android.util.Log
 import io.github.jan.supabase.postgrest.from
-import io.github.jan.supabase.postgrest.query.filter.PostgrestFilterBuilder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class FoodRepository(
     private val openFoodFactsApi: FoodApi
-// supabase client will be injected here
 ) {
+    // currently not using the openfoodfacts api
     suspend fun getProduct(barcode: String): ProductDetails? {
-        // forces execution to background thread pool
         return withContext(Dispatchers.IO) {
-            val offResponse = try {
+            try {
                 openFoodFactsApi.getProductByBarcode(barcode).product
             } catch (e: Exception) {
-                e.printStackTrace()
+                Log.e("FoodRepository", "Error fetching from OFF: ${e.message}")
                 null
             }
-
-            val offIngredients = offResponse?.ingredientsText
-
-            (if (!offIngredients.isNullOrBlank()) {
-                Log.d("FoodRepository", "found at OpenFoodFacts")
-                offResponse
-            } else {
-                Log.d("FoodRepository", "not found at OpenFoodFacts")
-                Log.d("FoodRepository", "checking database")
-                queryProduct(barcode)
-            }) as ProductDetails?
         }
     }
 
     suspend fun queryProduct(barcode: String): Product? {
-        return try {
-            SupabaseClient.client.from("products").select {
-                filter {
-                    val filter: PostgrestFilterBuilder = this
-                    filter.eq("barcode", barcode)
-                }
-            }.decodeSingle<Product>()
-        } catch (e: Exception) {
-            Log.d("FoodRepository", "supabase connection failed: ${e.message}")
-            null
+        return withContext(Dispatchers.IO) {
+            try {
+                SupabaseClient.client.from("products").select {
+                    filter {
+                        eq("barcode", barcode)
+                    }
+                }.decodeSingle<Product>()
+            } catch (e: Exception) {
+                Log.d("FoodRepository", "Supabase query failed: ${e.message}")
+                null
+            }
         }
     }
 }
