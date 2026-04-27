a9100edc6605e8ab23f6ed06ce60250cc3213991
Axyz7 <nautiyalaryan9406@gmail.com>
2026-04-27 21:14:30 +0530

    feat: add log

--- composeApp/src/androidMain/kotlin/org/troikas/main/network/FoodRepository.kt
+++ composeApp/src/androidMain/kotlin/org/troikas/main/network/FoodRepository.kt
@@ -9,12 +9,13 @@ class FoodRepository() {
     suspend fun queryProduct(barcode: String): Product? {
         return withContext(Dispatchers.IO) {
             try {
-                SupabaseClient.client.from("products").select {
+                val response = SupabaseClient.client.from("products").select {
                     filter {
                         eq("barcode", barcode)
                     }
                 }.decodeList<Product>()
-                    .firstOrNull()
+                Log.d("FoodRepository", "Supabase query successful: ${response.size} products found")
+                response.firstOrNull()
             } catch (e: Exception) {
                 Log.d("FoodRepository", "Supabase query failed: ${e.message}")
                 null
