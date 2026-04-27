b4d5d0bca6f4f27eaafb33bc644a49ed0d1be5cf
Axyz7 <nautiyalaryan9406@gmail.com>
2026-04-27 13:10:52 +0530

    fix: back to first approach to parse

--- composeApp/src/androidMain/kotlin/org/troikas/main/utils/StringParser.kt
+++ composeApp/src/androidMain/kotlin/org/troikas/main/utils/StringParser.kt
@@ -2,17 +2,22 @@ package org.troikas.main.utils
 
 class stringParser {
     fun stringParser(rawString: String): List<String> {
-        return Regex("\\((.*?)\\)")
-                .replace(rawString) { matchResult ->
-                    val textInside =
-                            matchResult.groupValues[
-                                    1] // numbers(if there are any) or whatever is nside the
-                    // brackets
-                    if (textInside.any { it.isDigit() }) ",$textInside"
-                    else "" // if it's a number, add it, or discard it
-                }
-                .split(",", "-", ":", " and ", " And ", " AND ")
+        return rawString
+                .replace("(", ", ")
+                .replace(")", ", ")
+                .split(
+                        ",",
+                        ".",
+                        "-",
+                        ":",
+                        " and ",
+                        " And ",
+                        " AND ",
+                        "contains",
+                        "CONTAINS",
+                        "Contains"
+                )
                 .map { it.trim().lowercase() }
-                .filter { it.isNotEmpty() }
+                .filter { it.isNotEmpty() && it != "permitted" && it != "added" }
     }
 }
