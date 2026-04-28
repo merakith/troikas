2cc3563d46a46a179f9086027e840a148cdc5415
Axyz7 <nautiyalaryan9406@gmail.com>
2026-04-28 15:42:17 +0530

    feat: add converter class for storing synonyms(list)

--- composeApp/src/androidMain/kotlin/org/troikas/main/database/AppDatabase.kt
+++ composeApp/src/androidMain/kotlin/org/troikas/main/database/AppDatabase.kt
@@ -11,6 +11,8 @@ import androidx.room.RoomDatabase
     version = 1, // if we add another column, we change this
     exportSchema = false // shuts up the compiler about missing schema folders
 )
+
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     // this abstract function tells the room to auto generate the code
     // to connect DAO queries to this specific database.
