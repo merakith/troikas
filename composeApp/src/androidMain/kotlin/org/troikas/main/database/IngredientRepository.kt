637752c44cad7d07f3562882373ab2091796cced
Axyz7 <nautiyalaryan9406@gmail.com>
2026-04-28 19:11:38 +0530

    feat: reimplement delta sync

--- composeApp/src/androidMain/kotlin/org/troikas/main/database/IngredientRepository.kt
+++ composeApp/src/androidMain/kotlin/org/troikas/main/database/IngredientRepository.kt
@@ -15,10 +15,14 @@ class IngredientRepository(
     suspend fun syncWithCloud() {
         withContext(Dispatchers.IO) {
             try {
-                // val lastSync = ingredientDao.getLastUpdatedAt() ?: "1970-01-01T00:00:00Z"
+                val lastSync = ingredientDao.getLastUpdatedAt() ?: "1970-01-01T00:00:00Z"
                 val response =
                         supabase.postgrest["ingredient_classification"]
-                                .select()
+                                .select {
+                                    filter {
+                                        gt("updated_at", lastSync) // greater than last sync time
+                                    }
+                                }
                                 .decodeList<IngredientClassification>()
                 if (response.isNotEmpty()) {
                     ingredientDao.insertIngredients(response)
